62177068300f787f4ae40b46488a1e24d4e79297
MiniX16 <javivi004.ou@gmail.com>
2025-03-16 18:29:53 +0100

    PIOT-GDA-02-002

--- src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
+++ src/main/java/programmingtheiot/gda/system/SystemPerformanceManager.java
@@ -1,60 +1,46 @@
-/**
- * This class is part of the Programming the Internet of Things project.
- * 
- * It is provided as a simple shell to guide the student and assist with
- * implementation for the Programming the Internet of Things exercises,
- * and designed to be modified by the student as needed.
- */ 
-
 package programmingtheiot.gda.system;
 
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
+import java.util.logging.Logger;
 import programmingtheiot.common.ConfigConst;
 import programmingtheiot.common.ConfigUtil;
 import programmingtheiot.common.IDataMessageListener;
-import programmingtheiot.common.ResourceNameEnum;
-import programmingtheiot.data.SystemPerformanceData;
 
 /**
  * Shell representation of class for student implementation.
- * 
  */
-public class SystemPerformanceManager
-{
-	// private var's
-	
-	
-	// constructors
-	
-	/**
-	 * Default.
-	 * 
-	 */
-	public SystemPerformanceManager()
-	{
-	}
-	
-	
-	// public methods
-	
-	public void handleTelemetry()
-	{
-	}
-	
-	public void setDataMessageListener(IDataMessageListener listener)
-	{
-	}
-	
-	public void startManager()
-	{
-	}
-	
-	public void stopManager()
-	{
-	}
-	
+public class SystemPerformanceManager {
+    private static final Logger _Logger = Logger.getLogger(SystemPerformanceManager.class.getName());
+    private int pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+
+    // Constructor
+    public SystemPerformanceManager() {
+        this.pollRate = ConfigUtil.getInstance().getInteger(
+            ConfigConst.GATEWAY_DEVICE,
+            ConfigConst.POLL_CYCLES_KEY,
+            ConfigConst.DEFAULT_POLL_CYCLES
+        );
+
+        if (this.pollRate <= 0) {
+            this.pollRate = ConfigConst.DEFAULT_POLL_CYCLES;
+        }
+    }
+
+    // Public methods
+    public void handleTelemetry() {
+        // Implement telemetry logic here
+    }
+
+    public void setDataMessageListener(IDataMessageListener listener) {
+        // Set the data message listener
+    }
+
+    public boolean startManager() {
+        _Logger.info("SystemPerformanceManager is starting...");
+        return true;
+    }
+
+    public boolean stopManager() {
+        _Logger.info("SystemPerformanceManager is stopped.");
+        return true;
+    }
 }
